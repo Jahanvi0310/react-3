a32796940f6a9e8bf5eb34357d0783673c79e1dd
Pulkit Anand <pulkitanand@Pulkit-Anands-MacBook-Pro.local>
2022-08-22 02:00:49 +0530

    ui done and functionality

---
+++ calculater-app/src/components/Input.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Input.css';
+
+const Input=({text,result})=>{
+    return(
+        <>
+<div className="inp">
+    <div className="result">
+    <h1>{result}</h1>
+    </div>
+    <div className="text">
+        <h1>{text}</h1>
+    </div>
+</div>
+        </>
+    )
+}
+export default Input;
